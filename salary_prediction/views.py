e5916c748966bb68773a1f4bb472a830808afe2d
Shashank07hr <shashank.agastya@icloud.com>
2026-02-08 12:54:30 +0530

    Fix salary prediction app and clean gitignore

--- salary_prediction/views.py
+++ salary_prediction/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
-import pickle
-import numpy as np
-import matplotlib.pyplot as plt
-
 from django.conf import settings
 from .forms import SalaryForm
 from .models import Prediction
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 
-#Loading the Model
+
+# Load ML model once
 model_path = os.path.join(
     settings.BASE_DIR,
     'salary_prediction',
     'model',
-    'salary_model.pkl',
+    'salary_model.pkl'
 )
-model = pickle.load(open(model_path, 'rb'))
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
 
 def home(request):
     form = SalaryForm()
-    return render(request, 'salary_prediction/home.html', {'form':form})
+    return render(request, 'salary_prediction/home.html', {'form': form})
+
 
 def predict_salary(request):
     if request.method == 'POST':
@@ -28,37 +33,39 @@ def predict_salary(request):
         if form.is_valid():
             exp = form.cleaned_data['experience']
 
-            exp_array = np.array(exp).reshape(-1,1)
-            salary = model.predict(exp_array)[0][0]
+            exp_array = np.array(exp).reshape(-1, 1)
+            predicted_salary = model.predict(exp_array)[0][0]
 
+        
             Prediction.objects.create(
-                experience = exp,
-                predict_salary = salary
+                experience=exp,
+                predicted_salary=predicted_salary
             )
 
-            #Plot
+            # Plot
             x_range = np.linspace(0, 15, 100)
-            y_pred = model.predict(x_range.reshape(-1,1))
+            y_pred = model.predict(x_range.reshape(-1, 1))
 
-            plt.figure(figsize=(8,6))
-            plt.plot(x_range, y_pred)
-            plt.scatter(exp , salary, marker='*')
-            plt.ylabel('Years of experience')
-            plt.xlabel('Salary (₹)')
-            plt.title('LR Prediction Line')
+            plt.figure(figsize=(8, 6))
+            plt.plot(x_range, y_pred, label='Prediction Line')
+            plt.scatter(exp, predicted_salary, color='red', marker='*', label='Input')
+            plt.xlabel('Years of Experience')
+            plt.ylabel('Salary (₹)')
+            plt.title('Salary Prediction')
+            plt.legend()
 
-            plot_path = os.path.join(
-                settings.MEDIA_ROOT,
-                'salary_plot.png'
-            )
-
+            plot_path = os.path.join(settings.MEDIA_ROOT, 'salary_plot.png')
             plt.savefig(plot_path)
-            plt.closer()
+            plt.close()
 
             context = {
-                'salary' : round(salary, 2),
                 'experience': exp,
+                'salary': round(predicted_salary, 2),
                 'plot_url': settings.MEDIA_URL + 'salary_plot.png'
             }
+
             return render(request, 'salary_prediction/result.html', context)
-        return  render(request, 'salary_prediction/home.html', {'form' : form})
+
+    
+    form = SalaryForm()
+    return render(request, 'salary_prediction/home.html', {'form': form})
